94a975057d32033db0f5ceb7bb5036c41693619b
Greg Swan <gmswan99@gmail.com>
2025-09-25 04:07:34 -0400

    Initial Commit

---
+++ Movie List/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Movie List
+//
+//  Created by Greg Swan on 9/25/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Movie List/Movie_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_ListApp.swift
+//  Movie List
+//
+//  Created by Greg Swan on 9/25/25.
+//
+
+import SwiftUI
+
+@main
+struct Movie_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
